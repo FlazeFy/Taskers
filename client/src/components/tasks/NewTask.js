0dca86f4ebb914d425883eddc77b11d152a29231
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2022-11-26 23:13:56 +0700

    Post task w/ file attachment and some fixes

--- client/src/components/tasks/NewTask.js
+++ client/src/components/tasks/NewTask.js
@@ -6,8 +6,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 //import { faSquareCheck, faClock, faUser } from "@fortawesome/free-regular-svg-icons";
 import { faPlus, faTrash, faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 
+
+import { storage } from '../../firebase';
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { useState, useEffect } from "react";
 import Axios from "axios";
+import {firestore} from "../../firebase";
 
 library.add( faTrash, faPlus, faArrowsRotate );
 
@@ -18,6 +22,26 @@ const NewTask = () => {
     const [prize, setPrize] = useState("");
     const [checklist, setChecklist] = useState([{}]);
     const [taglist, setTaglist] = useState([{}]);
+    const [attach, setAttList] = useState([{}]);
+    const [imgUrl, setImgUrl] = useState([]);
+    const [uploadFile, setUploadFile] = useState(null);
+    const [progresspercent, setProgresspercent] = useState(0);
+
+    //Attachment
+    const handleAddAtt = () => {
+        setAttList([...attach, {}]);
+    };
+
+    const handleRemoveAtt = (index) => {
+        const list = [...attach];
+        list.splice(index, 1);
+        setAttList(list);
+    };
+
+    const handleRemoveAllAtt = () => {
+        const list = [];
+        setAttList(list);
+    };
 
     //Checklist.
     const handleAddCheck = () => {
@@ -84,73 +108,131 @@ const NewTask = () => {
             console.log(error.response);
         }
     };
+
+    const handleSubmit = (e) => {
+        // e.preventDefault();
+        const file = uploadFile;
+    
+        if (!file) return;
+    
+        const storageRef = ref(storage, `task/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+    
+        uploadTask.on("state_changed",
+            (snapshot) => {
+                const progress =
+                Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                setProgresspercent(progress);
+            },
+            (error) => {
+                alert(error);
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                setImgUrl(downloadURL)
+                });
+            }
+        );
+      }
     
     const id_key = 1; //user id for now
 
     return (
         <div className='modal fade' id='new_task_modal' aria-labelledby='exampleModalLabel' aria-hidden='true'>
-            <div className='modal-dialog'>
+            <div className='modal-dialog modal-lg'>
                 <div className='modal-content border-0'>
                     <div className='modal-body position-relative px-3 py-4'>
                         <button type='button' className='btn-close position-absolute' data-bs-dismiss='modal' aria-label='Close'></button>
                         <h5 className='modal-title my-3 text-center' id='exampleModalLabel'>Add New Task</h5>
                         <form onSubmit={addNewTask}>
-                            <div className="form-floating mb-3">
-                                <input required type="text" className="form-control px-3" id="floatingInput" 
-                                    value={title} onChange={(e) => setTitle(e.target.value)}></input>
-                                <label className='px-3'> Task Title</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <textarea type="text" className="form-control px-3" id="floatingInput"
-                                    value={desc} onChange={(e) => setDesc(e.target.value)}></textarea>
-                                <label className='px-3'> Description <span className='position-absolute input-info-desc'>Optional</span></label>
-                            </div>
-                            <div className="form-floating mb-3 position-relative">
-                                <input type="text" className="form-control px-3" id="floatingInput"
-                                    value={prize} onChange={(e) => setPrize(e.target.value)}></input>
-                                <label className='px-3'> Prize/Result <span className='position-absolute input-info-price'>Optional</span></label>
-                            </div>
+                            <div className='row'>
+                                <div className='col-lg-6 col-md-12 col-sm-12'>
+                                    <div className="form-floating mb-3">
+                                        <input required type="text" className="form-control px-3" id="floatingInput" 
+                                            value={title} onChange={(e) => setTitle(e.target.value[0])}></input>
+                                        <label className='px-3'> Task Title</label>
+                                    </div>
+                                    <div className="form-floating mb-3">
+                                        <textarea type="text" className="form-control px-3" id="floatingInput"
+                                            value={desc} onChange={(e) => setDesc(e.target.value)}></textarea>
+                                        <label className='px-3'> Description</label>
+                                        <span className='position-absolute input-info-desc'>Optional</span>
+                                    </div>
+                                    <div className="form-floating mb-3 position-relative">
+                                        <input type="text" className="form-control px-3" id="floatingInput"
+                                            value={prize} onChange={(e) => setPrize(e.target.value)}></input>
+                                        <label className='px-3'> Prize/Result </label>
+                                        <span className='position-absolute input-info-price'>Optional</span>
+                                    </div>
+                                    <h6>Attachment</h6>
+                                    {
+                                        attach.map((singleService, index) => (
+                                            <div className='row mb-1' key={index}>
+                                                <div className='col-10'>
+                                                    <input type="file" className="form-control" onChange={(e) => setUploadFile(e.target.files)} onBlur={(e)=> handleSubmit()}></input>
+                                                </div>
+                                                <div className='col-2'>
+                                                    <a className='btn btn-delete h-75 w-75' title='Delete' onClick={handleRemoveAtt}><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                    <a id='btn_add_att' className='btn-add-new' onClick={handleAddAtt}><FontAwesomeIcon icon="fa-solid fa-add" /> Add Attachment</a>
+                                    <a id='btn_reset_att' className='btn-reset' onClick={handleRemoveAllAtt}><FontAwesomeIcon icon="fa-solid fa-arrows-rotate" /> Reset Attachment</a>
 
-                            <hr></hr>
-                            <h6>Checklist</h6>
-                            <div id='checklist_holder'>
-                                {checklist.map((singleService, index) => (
-                                <div className='row' key={index}>
-                                    <div className='col-10'>
-                                        <div className="form-floating mb-2">
-                                            <input type="text" className="form-control px-3" id="floatingInput" required
-                                                onChange={(e) => handleCheckChange(e, index)}></input>
-                                            <label className='px-3'><b>#{index+1}</b> Checklist Name</label>
+                                    {
+                                        !imgUrl &&
+                                        <div className='outerbar'>
+                                        <div className='innerbar' style={{ width: `${progresspercent}%` }}>{progresspercent}%</div>
                                         </div>
+                                    }
+                                    {
+                                        imgUrl &&
+                                        <img src={imgUrl} alt='uploaded file' height={200} />
+                                    }
+                                </div>
+                                <div className='col-lg-6 col-md-12 col-sm-12'>
+                                    <h6>Checklist</h6>
+                                    <div id='checklist_holder'>
+                                        {checklist.map((singleService, index) => (
+                                        <div className='row' key={index}>
+                                            <div className='col-10'>
+                                                <div className="form-floating mb-2">
+                                                    <input type="text" className="form-control px-3" id="floatingInput" required
+                                                        onChange={(e) => handleCheckChange(e, index)}></input>
+                                                    <label className='px-3'><b>#{index+1}</b> Checklist Name</label>
+                                                </div>
+                                            </div>
+                                            <div className='col-2 p-2'>
+                                                <a className='btn btn-delete h-75 w-75' title='Delete' onClick={handleRemoveCheck}><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
+                                            </div>
+                                        </div>))}
                                     </div>
-                                    <div className='col-2 p-2'>
-                                        <a className='btn btn-delete h-75 w-75' title='Delete' onClick={handleRemoveCheck}><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
-                                    </div>
-                                </div>))}
-                            </div>
-                            <a id='btn_add_check' className='btn-add-new' onClick={handleAddCheck}><FontAwesomeIcon icon="fa-solid fa-add" /> Add New Check</a>
-                            <a id='btn_reset_check' className='btn-reset' onClick={handleRemoveAllCheck}><FontAwesomeIcon icon="fa-solid fa-arrows-rotate" /> Reset Checklist</a>
+                                    <a id='btn_add_check' className='btn-add-new' onClick={handleAddCheck}><FontAwesomeIcon icon="fa-solid fa-add" /> Add New Check</a>
+                                    <a id='btn_reset_check' className='btn-reset' onClick={handleRemoveAllCheck}><FontAwesomeIcon icon="fa-solid fa-arrows-rotate" /> Reset Checklist</a>
 
-                            <hr></hr>
-                            <h6>Tags</h6>
-                            <div id='tags_holder'>
-                                {taglist.map((singleService, index) => (
-                                <div className='row' key={index}>
-                                    <div className='col-10'>
-                                        <div className="form-floating mb-2 position-relative">
-                                            <input type="text" className="form-control px-4" id="floatingInput" required
-                                                onChange={(e) => handleTagChange(e, index)}></input>
-                                            <label className='px-3'>Tag Name</label>
-                                            <span className='px-3 position-absolute tags-icon'>#</span>
-                                        </div>
+                                    <hr></hr>
+                                    <h6>Tags</h6>
+                                    <div id='tags_holder'>
+                                        {taglist.map((singleService, index) => (
+                                        <div className='row' key={index}>
+                                            <div className='col-10'>
+                                                <div className="form-floating mb-2 position-relative">
+                                                    <input type="text" className="form-control px-4" id="floatingInput" required
+                                                        onChange={(e) => handleTagChange(e, index)}></input>
+                                                    <label className='px-3'>Tag Name</label>
+                                                    <span className='px-3 position-absolute tags-icon'>#</span>
+                                                </div>
+                                            </div>
+                                            <div className='col-2 p-2'>
+                                                <a className='btn btn-delete h-75 w-75' title='Delete' onClick={handleRemoveTag}><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
+                                            </div>
+                                        </div>))}
                                     </div>
-                                    <div className='col-2 p-2'>
-                                        <a className='btn btn-delete h-75 w-75' title='Delete' onClick={handleRemoveTag}><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
-                                    </div>
-                                </div>))}
+                                    <a id='btn_add_tag' className='btn-add-new' onClick={handleAddTag}><FontAwesomeIcon icon="fa-solid fa-add" /> Add New Tag</a>
+                                    <a id='btn_reset_tag' className='btn-reset' onClick={handleRemoveAllTag}><FontAwesomeIcon icon="fa-solid fa-arrows-rotate" /> Reset Tags</a>
+                                </div>
                             </div>
-                            <a id='btn_add_tag' className='btn-add-new' onClick={handleAddTag}><FontAwesomeIcon icon="fa-solid fa-add" /> Add New Tag</a>
-                            <a id='btn_reset_tag' className='btn-reset' onClick={handleRemoveAllTag}><FontAwesomeIcon icon="fa-solid fa-arrows-rotate" /> Reset Tags</a>
                         <button type='submit' className='btn btn-success float-end' title='Save Task'><FontAwesomeIcon icon="fa-solid fa-add" /> Save</button>
                         </form>
                     </div>
